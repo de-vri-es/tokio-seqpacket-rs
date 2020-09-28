d7443d2ac8504743a0f7f8b44d0fc5afafb4e18b
Maarten de Vries <maarten@de-vri.es>
2020-09-28 21:11:05 +0200

    Add vectored IO.

--- src/unix_seqpacket.rs
+++ src/unix_seqpacket.rs
@@ -2,11 +2,22 @@ use std::path::Path;
 use tokio::future::poll_fn;
 use tokio::io::PollEvented;
 use tokio::net::unix::UCred;
+use std::io::{IoSlice, IoSliceMut};
 use std::task::{Context, Poll};
 use ::mio::Ready;
+use std::os::unix::io::AsRawFd;
 
 pub struct UnixSeqpacket {
 	io: PollEvented<crate::mio::EventedSocket>,
+}
+
+macro_rules! ready {
+	($e:expr) => {
+		match $e {
+			Poll::Pending => return Poll::Pending,
+			Poll::Ready(x) => x,
+		}
+	};
 }
 
 impl UnixSeqpacket {
@@ -62,14 +73,76 @@ impl UnixSeqpacket {
 		self.io.get_ref().take_error()
 	}
 
+	/// Send data on the socket to the connected peer without blocking.
+	pub fn poll_send(&mut self, cx: &mut Context, buffer: &[u8]) -> Poll<std::io::Result<usize>> {
+		ready!(self.io.poll_write_ready(cx)?);
+
+		match self.io.get_ref().send(buffer) {
+			Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+				self.io.clear_write_ready(cx)?;
+				Poll::Pending
+			}
+			x => Poll::Ready(x),
+		}
+	}
+
+	/// Send data on the socket to the connected peer without blocking.
+	pub fn poll_send_vectored(&mut self, cx: &mut Context, buffer: &[IoSlice]) -> Poll<std::io::Result<usize>> {
+		ready!(self.io.poll_write_ready(cx)?);
+
+		match send_msg(self.io.get_ref(), buffer) {
+			Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+				self.io.clear_write_ready(cx)?;
+				Poll::Pending
+			}
+			x => Poll::Ready(x),
+		}
+	}
+
 	/// Send data on the socket to the connected peer.
-	pub async fn send(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-		poll_fn(|cx| self.poll_send_priv(cx, buf)).await
+	pub async fn send(&mut self, buffer: &[u8]) -> std::io::Result<usize> {
+		poll_fn(|cx| self.poll_send(cx, buffer)).await
+	}
+
+	/// Send data on the socket to the connected peer.
+	pub async fn send_vectored(&mut self, buffer: &[IoSlice<'_>]) -> std::io::Result<usize> {
+		poll_fn(|cx| self.poll_send_vectored(cx, buffer)).await
+	}
+
+	/// Receive data on the socket from the connected peer without blocking.
+	pub fn poll_recv(&mut self, cx: &mut Context, buffer: &mut [u8]) -> Poll<std::io::Result<usize>> {
+		ready!(self.io.poll_read_ready(cx, Ready::readable())?);
+
+		match self.io.get_ref().recv(buffer) {
+			Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+				self.io.clear_read_ready(cx, Ready::readable())?;
+				Poll::Pending
+			},
+			x => Poll::Ready(x),
+		}
+	}
+
+	/// Receive data on the socket from the connected peer without blocking.
+	pub fn poll_recv_vectored(&mut self, cx: &mut Context, buffer: &mut [IoSliceMut]) -> Poll<std::io::Result<usize>> {
+		ready!(self.io.poll_read_ready(cx, Ready::readable())?);
+
+		match recv_msg(self.io.get_ref(), buffer) {
+			Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+				self.io.clear_read_ready(cx, Ready::readable())?;
+				Poll::Pending
+			},
+			x => Poll::Ready(x),
+		}
 	}
 
 	/// Receove data on the socket from the connected peer.
-	pub async fn recv(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-		poll_fn(|cx| self.poll_recv_priv(cx, buf)).await
+	pub async fn recv(&mut self, buffer: &mut [u8]) -> std::io::Result<usize> {
+		poll_fn(|cx| self.poll_recv(cx, buffer)).await
+	}
+
+	/// Receove data on the socket from the connected peer.
+	pub async fn recv_vectored(&mut self, buffer: &mut [IoSliceMut<'_>]) -> std::io::Result<usize> {
+		poll_fn(|cx| self.poll_recv_vectored(cx, buffer)).await
 	}
 
 	/// Shuts down the read, write, or both halves of this connection.
@@ -80,34 +153,47 @@ impl UnixSeqpacket {
 	pub fn shutdown(&self, how: std::net::Shutdown) -> std::io::Result<()> {
 		self.io.get_ref().shutdown(how)
 	}
+}
 
-	pub fn poll_send_priv(&self, cx: &mut Context, buf: &[u8]) -> Poll<std::io::Result<usize>> {
-		let ready = self.io.poll_write_ready(cx)?;
-		if ready.is_pending() {
-			return Poll::Pending;
-		}
+const SEND_MSG_DEFAULT_FLAGS: std::os::raw::c_int = libc::MSG_NOSIGNAL;
+const RECV_MSG_DEFAULT_FLAGS: std::os::raw::c_int = libc::MSG_NOSIGNAL | libc::MSG_CMSG_CLOEXEC;
 
-		match self.io.get_ref().send(buf) {
-			Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-				self.io.clear_write_ready(cx)?;
-				Poll::Pending
-			}
-			x => Poll::Ready(x),
-		}
+fn send_msg(socket: &socket2::Socket, buffer: &[IoSlice]) -> std::io::Result<usize> {
+	let fd = socket.as_raw_fd();
+	let header = libc::msghdr {
+		msg_name: std::ptr::null_mut(),
+		msg_namelen: 0,
+		msg_iov: buffer.as_ptr() as *mut libc::iovec,
+		msg_iovlen: buffer.len(),
+		msg_flags: 0,
+		msg_control: std::ptr::null_mut(),
+		msg_controllen: 0,
+	};
+	unsafe {
+		check_returned_size(libc::sendmsg(fd, &header as *const _, SEND_MSG_DEFAULT_FLAGS))
 	}
+}
 
-	pub fn poll_recv_priv(&self, cx: &mut Context, buf: &mut [u8]) -> Poll<std::io::Result<usize>> {
-		let ready = self.io.poll_read_ready(cx, Ready::readable())?;
-		if ready.is_pending() {
-			return Poll::Pending;
-		}
+fn recv_msg(socket: &socket2::Socket, buffer: &mut [IoSliceMut]) -> std::io::Result<usize> {
+	let fd = socket.as_raw_fd();
+	let mut header = libc::msghdr {
+		msg_name: std::ptr::null_mut(),
+		msg_namelen: 0,
+		msg_iov: buffer.as_ptr() as *mut libc::iovec,
+		msg_iovlen: buffer.len(),
+		msg_flags: 0,
+		msg_control: std::ptr::null_mut(),
+		msg_controllen: 0,
+	};
+	unsafe {
+		check_returned_size(libc::recvmsg(fd, &mut header as *mut _, RECV_MSG_DEFAULT_FLAGS))
+	}
+}
 
-		match self.io.get_ref().recv(buf) {
-			Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-				self.io.clear_read_ready(cx, Ready::readable())?;
-				Poll::Pending
-			}
-			x => Poll::Ready(x),
-		}
+fn check_returned_size(ret: isize) -> std::io::Result<usize> {
+	if ret < 0 {
+		Err(std::io::Error::last_os_error())
+	} else {
+		Ok(ret as usize)
 	}
 }
